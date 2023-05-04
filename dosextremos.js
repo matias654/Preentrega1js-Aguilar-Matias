bcd40bbc6ab68cf2b7e1913ce12454e1af65f298
matias <teagrosrl@gmail.com>
2023-05-03 23:57:18 -0300

    preentrega2

--- dosextremos.js
+++ dosextremos.js
@@ -1,4 +1,4 @@
-let producto = prompt(` ingrese numero de producto :
+/* let producto = prompt(` ingrese numero de producto :
  1:Remeras Unisex
  2:Buzos Unisex
  3:Camperas Unisex
@@ -61,4 +61,122 @@ solicitarDatosdeContacto();
 function saludar() {  
     console.log( "Gracias por su Contacto");
 }
+saludar(); */
+
+/*  Comienzo preentrega 2 *****************************/
+
+
+
+const articulo = [
+    { producto : "remera", precio: 4999 },
+    { producto:"buzo", precio: 8300 },
+    { producto:"campera", precio: 10500 },
+];
+
+
+let seleccionproducto = prompt(` ingrese  producto :
+Remeras Unisex
+Buzos Unisex
+Camperas Unisex
+`);
+
+
+let selecciontalle = prompt(`ingrese talle :
+ S: Small 
+ M: Medium
+ L: Large
+ XL: Extra Large
+`);
+
+switch (selecciontalle)
+ {
+    case "S": 
+    console.log( "usted selecciono talle Small");
+    break;
+    case "s": 
+    console.log( "usted selecciono talle Small");
+    break;
+    case "M": 
+    console.log( "usted selecciono talle Medium");
+    break;
+    case "m": 
+    console.log( "usted selecciono talle Medium");
+    break;
+    case "L": 
+    console.log( "usted selecciono talle Largue");
+    break;
+    case "l": 
+    console.log( "usted selecciono talle Largue");
+    break;
+    case "XL": 
+    console.log( "usted selecciono talle Extra Large");
+    break;
+    case "xl": 
+    console.log( "usted selecciono talle Extra Large");
+    break;
+default:
+    console.log( "Lo sentimos no tenemos esa opcion");
+    break; 
+
+
+}
+
+
+    if (seleccionproducto == "remera" || seleccionproducto== "buzo" || seleccionproducto== "campera") {
+ switch (seleccionproducto)
+  {
+         case "remera" :
+            precio= 4999 ;
+            console.log( "usted selecciono Remera");
+            break ;
+            case "buzo" :
+                precio= 8500 ;
+                console.log( "usted selecciono buzo");
+                break ;
+                case "campera" :
+                    precio= 10500 ;
+                    console.log( "usted selecciono campera");
+                    break ;
+                    default : 
+                    console.log( "Lo sentimos no tenemos esa opcion");
+                    break 
+
+
+ }
+
+ let carrito =[];
+
+
+ let pedido = parseInt (prompt (" ingrese cantidad de pedido" )) ;
+carrito.push({seleccionproducto,pedido,selecciontalle  });
+console.log(carrito)
+
+
+
+carrito.forEach((carritoTotal) => { 
+    console.log(`pedido : ${carritoTotal.pedido}, total a pagar : ${precio * carritoTotal.pedido},
+    
+ `)
+})
+
+
+};
+
+
+function solicitarDatosdeContacto () { let DatosdeContacto= prompt ( "Ingrese Nombre y Apellido")
+let Mail = prompt ("Ingrese Mail")
+let telefono = prompt ("Ingrese numero de telefono")
+console.log ("Los datos para que te contactemos son : " + DatosdeContacto +" "+  Mail + " " + telefono )
+
+}
+solicitarDatosdeContacto();
+
+
+function saludar() {  
+    alert( "Gracias por tu compra, en la brevedad te estaremos contactando para confirmar pedido y  plazos de entrega. ");
+}
 saludar();
+
+
+ 
+ 
